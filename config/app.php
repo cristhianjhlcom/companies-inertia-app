21c775a237f400dbbb41111fe7dd8f4bd87c0629
cristhianjhl.com <cristhianjhl.web@gmail.com>
2024-06-15 23:13:54 -0500

    add digital ocean url domain

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://urchin-app-gbup7.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
